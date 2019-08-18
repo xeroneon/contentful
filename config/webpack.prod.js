4ada435505a7b446061b95d26053a3674e99c266
Andrew Vasquez <andrewvasquez0113@gmail.com>
2019-08-18 13:03:12 -0700

    removed optimization, keeps crashing

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -14,17 +14,17 @@ module.exports = merge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  optimization: {
-    minimizer: [new TerserPlugin({
-      cache: true,
-      parallel: true,
-      sourceMap: true,
-      terserOptions: {
-        ecma: 6,
-        compress: false,
-        mangle: true
-      }
-    })]
+  // optimization: {
+  //   minimizer: [new TerserPlugin({
+  //     cache: true,
+  //     parallel: true,
+  //     sourceMap: true,
+  //     terserOptions: {
+  //       ecma: 6,
+  //       compress: false,
+  //       mangle: true
+  //     }
+  //   })]
     // [
     //   // we specify a custom UglifyJsPlugin here to get source maps in production
     //   new UglifyJsPlugin({
